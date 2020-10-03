b21934014b125e55644edc1b12aa6e26cc147b04
Benji <begr5293@colorad.edu>
2020-10-03 00:11:34 -0600

    Initial Project Setup

---
+++ MagicGunzPrototype/Assets/MainCameraScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainCameraScript : MonoBehaviour
+{
+    public int movespeed = 5;
+    public int diffdistance = 2;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Get player position in game world
+        Vector3 p_pos = GameObject.FindGameObjectWithTag("Player").transform.position;
+
+        // Compute the measured distance between camera and player using x,y position
+        Vector3 difference = p_pos - transform.position;
+        float measdistance = Mathf.Sqrt((difference.x * difference.x) + (difference.y * difference.y));
+
+        // Check to see if camera is too far away from player and move towards player if this is the case
+        if (measdistance > diffdistance)
+        {
+            // Normalize difference to get direction towards player
+            Vector3 dir = Vector3.Normalize(difference);
+            // Translate in direction towards player
+            transform.Translate(movespeed * Time.deltaTime * new Vector3(dir.x,dir.y,0));
+        }
+        
+    }
+}
---
+++ MagicGunzPrototype/Assets/PlayerMovement.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [Header("Movement Attributes")]
+    // movement direction based on controller input
+    public Vector2 moveDirection;
+    // movement speed based on controller input intensity
+    public float moveSpeed;
+
+    [Space]
+    [Header("Character Attributes")]
+    // base movement speed
+    public float baseSpeed = 1.0f;
+
+    [Space]
+    [Header("Reference Components")]
+    // rigidbody 2d class for interactions
+    public Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // read controller inputs
+        ProcessInputs();
+        // translate character
+        Movement();
+    }
+
+    // This function processes the controller input
+    void ProcessInputs()
+    {
+        // Get vector from controller input
+        moveDirection = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        // Base the movement speed on the intensity of controller input, clamp magnitude to be between 0 and 1
+        moveSpeed = Mathf.Clamp(moveDirection.magnitude, 0.0f, 1.0f);
+        moveDirection.Normalize();
+    }
+
+    // This function acts on the controller input and is called after ProcessInputs() and makes the player move
+    void Movement()
+    {
+        rb.velocity = moveDirection * moveSpeed * baseSpeed;
+    }
+}
